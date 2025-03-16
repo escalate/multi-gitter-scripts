a3ada2a4d22aa49515daa88df5784a3c5cce39a7
Felix Boerner <ich@felix-boerner.de>
2025-03-16 19:07:46 +0100

    style: use shfmt format

--- jobs/contributing/contributing.sh
+++ jobs/contributing/contributing.sh
@@ -5,5 +5,5 @@ echo "[$(date '+%Y-%m-%d %H:%M:%S')] Git repository: ${REPOSITORY}"
 
 echo "${REPOSITORY} - Override CONTRIBUTING.md"
 curl --silent --show-error --fail-with-body "https://raw.githubusercontent.com/escalate/multi-gitter-scripts/refs/heads/master/jobs/contributing/CONTRIBUTING.md" --output CONTRIBUTING.md
-envsubst < CONTRIBUTING.md > CONTRIBUTING.md.tmp
+envsubst <CONTRIBUTING.md >CONTRIBUTING.md.tmp
 mv CONTRIBUTING.md.tmp CONTRIBUTING.md
