199a4f79b227772327c092dbcefc3ddb39d8af52
Felix Boerner <ich@felix-boerner.de>
2025-05-22 11:03:05 +0200

    feat: remove dependabot config

--- jobs/renovate/renovate.sh
+++ jobs/renovate/renovate.sh
@@ -5,3 +5,6 @@ echo "[$(date '+%Y-%m-%d %H:%M:%S')] Git repository: ${REPOSITORY}"
 
 echo "${REPOSITORY} - Override renovate.json"
 curl --silent --show-error --fail-with-body "https://raw.githubusercontent.com/escalate/multi-gitter-scripts/refs/heads/master/jobs/renovate/recommended-renovate.json" --output renovate.json
+
+echo "${REPOSITORY} - Remove dependabot.yml"
+rm ".github/dependabot.yml"
