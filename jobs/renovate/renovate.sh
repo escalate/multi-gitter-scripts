dd989aa44180f989e3a74babb83bb8ce95c733eb
Felix Boerner <ich@felix-boerner.de>
2025-05-22 09:52:07 +0200

    feat: add renovate job

---
+++ jobs/renovate/renovate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e -E -u -C -o pipefail
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Git repository: ${REPOSITORY}"
+
+echo "${REPOSITORY} - Override renovate.json"
+curl --silent --show-error --fail-with-body "https://raw.githubusercontent.com/escalate/multi-gitter-scripts/refs/heads/master/jobs/renovate/recommended-renovate.json" --output renovate.json
